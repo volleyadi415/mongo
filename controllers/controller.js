5cefe675d8160dc5f5e159c1a1e8d8326a87e955
volleyadi415 <jhaa241@gmail.com>
2022-04-18 11:30:59 +0530

    crud

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,17 @@
-const MongoClient = require("../db/db");
-const url = "mongodb://localhost:27017/";
+
+const MongoClient = require('mongodb').MongoClient
+
+const connectionString = "mongodb://127.0.0.1/mydb"
 
 
 class Controller {
-    static async controller(req, res) {
+    static async collection(req, res) {
         try {
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                const dbo = db.db("test");
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                var dbo = db.db("mydb");
+
 
                 dbo.createCollection("customers", function (err, res) {
                     if (err) throw err;
@@ -15,56 +19,53 @@ class Controller {
                     db.close();
                 });
             });
-        } catch (e) {
+        } catch  (e) {
+            console.log(e)
 
         }
     }
 
-    static async controller2(req, res) {
+    static async insert(req, res) {
         try {
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("test");
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                
+
+                var dbo = db.db("mydb");
                 var myobj = [
-                    { name: 'adi', address: 'Highway 71' },
-                    { name: 'Pmit', address: 'Lowstreet 4' },
-                    { name: 'john', address: 'Apple st 652' },
-                    { name: 'gourav', address: 'Mountain 21' },
-                    { name: 'sourav', address: 'Valley 345' },
-                    { name: 'Sandy', address: 'Ocean blvd 2' },
-                    { name: 'Betty', address: 'Green Grass 1' },
-                    { name: 'Richard', address: 'Sky st 331' },
-                    { name: 'Susan', address: 'One way 98' },
-                    { name: 'Vicky', address: 'Yellow Garden 2' },
-                    { name: 'Ben', address: 'Park Lane 38' },
-                    { name: 'William', address: 'Central st 954' },
-                    { name: 'Chuck', address: 'Main Road 989' },
-                    { name: 'Viola', address: 'Sideway 1633' }
+                    { name: 'adi' },
+                    { name: 'Ben'  },
+                    { name: 'Willia'  },
+                    { name: 'Chuck'  },
+                    { name: 'Viola'  }
                 ];
                 dbo.collection("customers").insertMany(myobj, function (err, result) {
                     if (err) throw err;
                     console.log("Number of documents inserted: " + result.insertedCount);
                     res.send(result)
 
-                    db.close();
+                    
                 });
             });
         } catch (e) {
 
         }
     }
-    static async controller3(req, res) {
+    static async select(req, res) {
         try {
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("test");
-                dbo.collection("customers").find({}, { projection: { _id: 0, name: 1, address: 1 } }, function (err, result) {
-                    if (err) throw err;
-                    console.log(result);
-                    res.send(result)
-                    db.close();
-                });
+          
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                var dbo = db.db("mydb");
+            dbo.collection("customers").find({}).toArray(function(err, result) {
+              if (err) throw err;
+              console.log(result);
+              res.send(result);
             });
+              })
+            
         } catch (e) {
 
         }
@@ -72,18 +73,21 @@ class Controller {
 
     //Filter the Result
 
-    static async controller4(req, res) {
+    static async filter(req, res) {
         try {
 
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("test");
-                var query = { address: "Park Lane 38" };
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                var dbo = db.db("mydb");
+
+                
+                var query = { name: "adi" };
                 dbo.collection("customers").find(query).toArray(function (err, result) {
                     if (err) throw err;
                     console.log(result);
                     res.send(result);
-                    db.close();
+                    
                 });
             });
         } catch (e) {
@@ -93,18 +97,20 @@ class Controller {
 
     //sort result ascending(1) or descending(-1)
 
-    static async controller5(req, res) {
+    static async sort(req, res) {
         try {
 
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("test");
-                var mysort = { name: 1 };
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                var dbo = db.db("mydb");
+
+                var mysort = { name: aditya };
                 dbo.collection("customers").find().sort(mysort).toArray(function (err, result) {
                     if (err) throw err;
                     console.log(result);
                     res.send(result)
-                    db.close();
+                
                 });
             });
         } catch (e) {
@@ -114,18 +120,20 @@ class Controller {
 
     //delete document
 
-    static async controller6(req, res) {
+    static async delete(req, res) {
         try {
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("test");
-                var query = { address: 'Mountain 21' };
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                var dbo = db.db("mydb");
+
+                var query = { name: 'aditya' };
                 dbo.collection("customers").deleteMany(query, function (err, obj) {
 
                     if (err) throw err;
                     console.log(obj);
                     res.send(obj)
-                    db.close();
+                
                 });
             });
         } catch (e) {
@@ -134,13 +142,15 @@ class Controller {
     }
 
     // update document
-    static async controller7(req, res) {
+    static async update(req, res) {
         try {
 
-            MongoClient.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("test");
-                var myquery = { address: /^S/ };
+            MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, db) => {
+                if (err) return console.error(err)
+                console.log('Connected to Database')
+                var dbo = db.db("mydb");
+
+                var myquery = { name: "aditya" };
                 var newvalues = { $set: { name: "Minnie" } };
                 dbo.collection("customers").updateMany(myquery, newvalues, function (err, result) {
                     if (err) throw err;
--- routes/router.js
+++ routes/router.js
@@ -2,19 +2,19 @@ const express = require("express");
 const controller = require("../controllers/controller");
 const router = express.Router();
 
-router.get("/collection",controller.controller);
+router.get("/collection",controller.collection);
 
-router.post("/insert",controller.controller2)
+router.post("/insert",controller.insert)
 
-router.get("/select",controller.controller3)
+router.get("/select",controller.select);
 
-router.get("/filter",controller.controller4)
+router.get("/filter",controller.filter)
 
-router.get("/sort",controller.controller5)
+router.get("/sort",controller.sort)
 
-router.delete("/delete",controller.controller6)
+router.delete("/delete",controller.delete)
 
-router.put("/update",controller.controller6)
+router.put("/update",controller.update)
 
 
 
